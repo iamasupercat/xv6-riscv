6b8a1d079e3a8cee640959261cccab71ab3725e4
iamasupercat <caesium13755@gmail.com>
2025-11-05 20:03:27 +0900

    pa3

--- kernel/defs.h
+++ kernel/defs.h
@@ -174,6 +174,7 @@ int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
 int             ismapped(pagetable_t, uint64);
 uint64          vmfault(pagetable_t, uint64, int);
+void            fork_mmaps(struct proc *, struct proc *);
 
 // plic.c
 void            plicinit(void);
